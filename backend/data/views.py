11d2a34e63d4b36234b72e8bd6b5d7e1d21273c4
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-03-09 22:49:34 +0800

    new commit

--- backend/data/views.py
+++ backend/data/views.py
@@ -5,16 +5,31 @@ from backend.function import check_login, check_method, generate_token, \
     get_username_by_token, get_post_json
 import json
 from django.conf import settings
-
+from pyecharts import options as opts
+from pyecharts.charts import Pie
+from pyecharts.faker import Faker
 # 颜色和label
-hacker_list = ["0xngbqzb1kqrlt2shlg2qf9kccpg9suxmp0wwg4q3l", "0xx2zfq2etghz5fk6nd9z23g2gdugmp02fhq2r3s1t", "0xfgv113xtch13o0xxmf6cyyrck4s3znzmpt7vrhkw", "0x6o5v6hnm6rb0v330mwowsvwxpssqw7ry8rrxvt27",
-"0xq9165xhiq9zc19syggqid8qorcd0hf4aqg9stbf7", "0xcwy0z29i4ybxwkyu0c18xamyauvgmndh5hivkd6u", "0xu1hdwc6cgr7cnv8xo8dvyvz6e2mwrp3rxtl7z9h5", "0x12toapnwhlkqzddrz2wlsesignw9xkg5pamziuhb",
-"0xlg2kqu9f7htvh2ux2dmgbvk1tw3vzkbgzim16wv4", "0x8epddqq2h4421ul1zhfovgg1gy5uamhryqd3dw0x", "0xv2ki4alnx4yb2yenpbz29i1g4l5wdeksefgygnpi",
-"0x2imicclza5go3i5486ysxik00rubwft3x5qvdtbo", "0xzrbay9r4h0ig96btyz421cxwwplwuye9hktgcei6", "0x84bz5s4f7ecde5z9rhw36mo9kytz3g2rg4db8wad",
-"0xco21vlgq2mtsc0qk64cpuvpbh2kmc074iwy8k7k9", "0xto5zrnn8qgits71gxmmdfl3bggfz8q40inywgs06", "0xek1uci40xwi6y7aq9i2ghrs2uru0koepwvu8p32o",
+hacker_list = [
+    "0x12toapnwhlkqzddrz2wlsesignw9xkg5pamziuhb",
+    "0xlg2kqu9f7htvh2ux2dmgbvk1tw3vzkbgzim16wv4",  
+    "0xto5zrnn8qgits71gxmmdfl3bggfz8q40inywgs06", 
+    "0xek1uci40xwi6y7aq9i2ghrs2uru0koepwvu8p32o"
 ]
 
 hacker_list_no_icon = [
+    "0xu1hdwc6cgr7cnv8xo8dvyvz6e2mwrp3rxtl7z9h5",
+    "0xq9165xhiq9zc19syggqid8qorcd0hf4aqg9stbf7",
+    "0xzrbay9r4h0ig96btyz421cxwwplwuye9hktgcei6", 
+    "0x2imicclza5go3i5486ysxik00rubwft3x5qvdtbo",
+    "0xco21vlgq2mtsc0qk64cpuvpbh2kmc074iwy8k7k9",
+    "0xv2ki4alnx4yb2yenpbz29i1g4l5wdeksefgygnpi",
+    "0x8epddqq2h4421ul1zhfovgg1gy5uamhryqd3dw0x",
+    "0x84bz5s4f7ecde5z9rhw36mo9kytz3g2rg4db8wad",
+    "0xcwy0z29i4ybxwkyu0c18xamyauvgmndh5hivkd6u",
+    "0x6o5v6hnm6rb0v330mwowsvwxpssqw7ry8rrxvt27",
+    "0xx2zfq2etghz5fk6nd9z23g2gdugmp02fhq2r3s1t", 
+    "0xfgv113xtch13o0xxmf6cyyrck4s3znzmpt7vrhkw",
+    "0xngbqzb1kqrlt2shlg2qf9kccpg9suxmp0wwg4q3l", 
     "0xtqc65crrf0kubu0srryz8p6ly53gueua4327xbyo",
     "0xas1mdflghu6bhp7aek82xgrlhpdecflcg3x8gg6c",
     "0xh5x5akaqcazn4d02co6h5rfi6fulbxgzc5wbokdg",
@@ -35,19 +50,46 @@ hacker_list_no_icon = [
     "0xb7f68d5y61is14fcsdp5oah1gtaxb2atsdnri2ts",
     "0x64tyyubdfoi9o9u4fb86p265tuvruwobb25rtge4"
 ]
-cash_list = ["0xv290h0knxrksy39vt3m78rnpvkbeu291wlm23zaq", "0xf1r2wf53o45mqg6empwyy58mdlg559tvbbv969r1"]
+cash_list = [
+    "0xv290h0knxrksy39vt3m78rnpvkbeu291wlm23zaq", 
+    "0xf1r2wf53o45mqg6empwyy58mdlg559tvbbv969r1"]
+
+compromised_account_list = [
+    "0x78hnrbubw5688gyymwwnwuhamruq5b6t67utldpg"
+]
+
+compromised_account_list_no_label = [
+    "0xgqkw5myorew58nmzg8syxi2n7ptdg92ye8y182di",
+    "0xbvdywe6mgb2vdzth6612v0mc48sgdzypslxow4y9",
+    "0xruewzunvdg5fu5ec6manwl9a9ygu96pgo1t8gh0w",
+    "0xgnio6knh6g19n1kr2c0ghm0h4o2f5euxxgq7qsg2",
+    "0x692emmr9v2h7mwxmx3crlgrmqgcvwyu7ox3edoy7",
+    "0xu44g25s4vnqbuxnfsm0poiy31ind6za5gyww17ig",
+    "0x6vfr6tofrkbst1l7i1v0idwagte8stz6t04vu1gh",
+    "0x021cgduyftudedt7pyuftwmkrv697a7lb4wp75v4",
+    "0xrdio71fas6lhdii52w5qsyoods7kpkbqo7oximgw",
+    "0xnx12ao5hssauky5q0tdv8ga8giqz29acb6nzsshy",
+    "0xdnyyriydkcvbpz7u8yc7vxizl8a2916vxa0e757r"
+]
+
 def get_label(node_id):
     if node_id in hacker_list:
         return True, "Hacker Wallet"
     if node_id in cash_list:
         return True, "Tornado.Cash"
+    if node_id in compromised_account_list:
+        return True, "Compromised Account"
     return False, ""
 
 def get_color(node_id):
     return '#00BFFF'
+
 def get_size(degree):
     default_node_size = 10
-    return default_node_size + 1.5 * int(degree)
+    if degree == 1:
+        return default_node_size + 4 * int(degree)
+    return default_node_size + 2 * int(degree)
+
 @check_method('POST')
 def analyze_view(request):
     with open("./data/position.txt", encoding="utf-8") as f:
@@ -108,6 +150,14 @@ def analyze_view(request):
                 "height": node["size"] * 0.8
             }
             continue
+        if node["id"] in compromised_account_list_no_label:
+            node["icon"] = {
+                "show": True,
+                "img": "https://raw.githubusercontent.com/Liuyushiii/img/master/wallet4.png",
+                "width": node["size"] * 0.8,
+                "height": node["size"] * 0.8
+            }
+            continue    
         have_label, label = get_label(node["id"])
         if have_label:
             node["label"] = label
@@ -134,6 +184,14 @@ def analyze_view(request):
                     "width": node["size"] * 0.8,
                     "height": node["size"] * 0.8
                 }
+            elif label == "Compromised Account":
+                node["icon"] = {
+                    "show": True,
+                    "img": "https://raw.githubusercontent.com/Liuyushiii/img/master/wallet4.png",
+                    "width": node["size"] * 0.8,
+                    "height": node["size"] * 0.8
+                }
+                
     return JsonResponse({
         'message': 'ok',
         "nodes": nodes,
@@ -147,9 +205,49 @@ def overview_view(request):
     with open("./data/overview-edges.json", encoding="utf-8") as f:
         edges = json.load(f)
         f.close()
+    # degree = {"1-5": 0, "6-10": 0, ">10" : 0}
+    # for node in nodes:
+    #     if node["degree"] <= 5:
+    #         degree["1-5"] += 1
+    #     elif node["degree"] <= 10:
+    #         degree["6-10"] += 1
+    #     else:
+    #         degree[">10"] += 1
+    # degree_list = [{"name": key, "value": degree[key]} for key in degree]
+    degree_list = [
+        {
+            "name": "1-5",
+            "value": 123
+        },
+        {
+            "name": "6-10",
+            "value": 94
+        },
+        {
+            "name": ">10",
+            "value": 49
+        }
+    ]
+    # c = (
+    #     Pie()
+    #     .add(
+    #         "",
+    #         degree_list,
+    #         radius=["40%", "75%"],
+    #     )
+    #     .set_global_opts(
+    #         title_opts=opts.TitleOpts(title="Degree Distribution"),
+    #         legend_opts=opts.LegendOpts(orient="vertical", pos_top="15%", pos_left="2%"),
+    #     )
+    #     .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
+    #     # .render("pie_radius.html")
+    # )
+    # json_content = json.loads(c.dump_options())
+    # json_content["legend"]["left"] = "5%"
     return JsonResponse({
         'message': 'ok',
         "nodes": nodes,
-        "edges": edges
+        "edges": edges,
+        "json": degree_list
     })
     
