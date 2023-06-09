df881de3e3e6805d768953e3e450906ae560759a
ZhmYe <576770123@qq.com>
2023-06-09 13:24:15 +0800

    new commit

--- backend/data/views.py
+++ backend/data/views.py
@@ -287,17 +287,6 @@ def overview_view(request):
     with open("./data/overview-edges.json", encoding="utf-8") as f:
         edges = json.load(f)
         f.close()
-    # with open('./data/position.json', encoding="utf-8") as f:
-    #     node_position = json.load(f)
-    # position_dic = {}
-    # for node_info in node_position:
-    #     position_dic[node_info["id"]] = {"x": node_info["x"], "y": node_info["y"]}
-    # with open("./data/overview-nodes.json", encoding="utf-8") as f:
-    #     nodes = json.load(f)
-    #     f.close()
-    # with open("./data/overview-edges.json", encoding="utf-8") as f:
-    #     edges = json.load(f)
-    #     f.close()
     degree = {"1-5": 0, "6-10": 0, ">10" : 0}
     
     
@@ -311,8 +300,8 @@ def overview_view(request):
     
     for node in nodes:
         node["size"] = 10 + get_size_overview(node["degree"], 2)
-        # node["x"] = position_dic[node["id"]]["x"]
-        # node["y"] = position_dic[node["id"]]["y"]
+        node["x"] = position_dic[node["id"]]["x"]
+        node["y"] = position_dic[node["id"]]["y"]
         if node["degree"] <= 5:
             degree["1-5"] += 1
         elif node["degree"] <= 10:
